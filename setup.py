21209c8590e373125fdffd1ec32fa73f93a04c5f
Matthew Downs <matthew@neer.ai>
2026-01-02 17:08:45 -0600

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="swmm-utils",
-    version="0.2.0",
+    version="0.2.1",
     author="NEER",
     author_email="support@neer.ai",
     description="Utilities for encoding and decoding EPA SWMM input (.inp) and report (.rpt) files to/from multiple formats",
