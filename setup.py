8e210683247eea4a938407bcac21f8022770730d
elango <elango@neer.ai>
2026-09-24 15:49:44 -0500

    release: 1.4.0
    Minor bump: the results cube is chunked by period by default (a map
    frame is one small object); chunk_periods and target_chunk_bytes on
    emit_results_zarr, and the descriptor reports the chunks written.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="swmm-utils",
-    version="1.3.0",
+    version="1.4.0",
     author="NEER",
     author_email="support@neer.ai",
     description=description,
